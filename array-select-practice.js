2e9ee29dd8e62236f80c5eff0efdd8f12b6e5ac3
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-08-17 14:49:02 -0500

    practicing array select using filter function

--- array-select-practice.js
+++ array-select-practice.js
@@ -246,3 +246,105 @@
 //   }
 // });
 // console.log(oddNumbers);
+
+// 1. Start with an array of numbers and create a new array with only the numbers less than 20.
+//  For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
+
+// var numbers = [2, 32, 80, 18, 12, 3];
+// var selectedNumbers = numbers.filter(function (number) {
+//   return number < 20;
+// });
+
+// console.log(selectedNumbers);
+
+//  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
+//     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
+
+// var strings = ["winner", "winner", "chicken", "dinner"];
+// var words = strings.filter(function (word) {
+//   return word[0] === "w";
+// });
+
+// console.log(words);
+
+//  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
+//     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
+
+// var hashes = [
+//   { name: "chair", price: 100 },
+//   { name: "pencil", price: 1 },
+//   { name: "book", price: 4 },
+// ];
+// var greaterthanFive = hashes.filter(function (item) {
+//   return item.price > 5;
+// });
+
+// console.log(greaterthanFive);
+
+//  4. Start with an array of numbers and create a new array with only the even numbers.
+//     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
+
+// var numbers = [2, 4, 5, 1, 8, 9, 7];
+// var evenNumbers = numbers.filter(function (number) {
+//   return number % 2 === 0;
+// });
+
+// console.log(evenNumbers);
+
+//  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
+//     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
+
+// var strings = ["a", "man", "a", "plan", "a", "canal", "panama"];
+// var shortest = strings.filter(function (string) {
+//   return string.length < 4;
+// });
+
+// console.log(shortest);
+
+//  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
+//     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
+
+// var items = [
+//   { name: "chair", price: 100 },
+//   { name: "pencil", price: 1 },
+//   { name: "book", price: 4 },
+// ];
+// var shortestName = items.filter(function (item) {
+//   return item.name.length < 6;
+// });
+
+// console.log(shortestName);
+
+//  8. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
+//     For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
+
+// var strings = ["big", "little", "good", "bad"];
+// var bWords = strings.filter(function (string) {
+//   return string[0] !== "b";
+// });
+
+// console.log(bWords);
+
+//  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
+//     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
+
+// var items = [
+//   { name: "chair", price: 100 },
+//   { name: "pencil", price: 1 },
+//   { name: "book", price: 4 },
+// ];
+// var cheapest = items.filter(function (item) {
+//   return item.price < 10;
+// });
+
+// console.log(cheapest);
+
+// 10. Start with an array of numbers and create a new array with only the odd numbers.
+//     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
+
+// var numbers = [2, 4, 5, 1, 8, 9, 7];
+// var odds = numbers.filter(function (number) {
+//   return number % 2 === 1;
+// });
+
+// console.log(odds);
