5021d3f34c35da3f1b7f87b927d1b1e343a2e0a2
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-09-06 13:22:40 -0500

    solving reverse number

---
+++ reverse-number.js
@@ -0,0 +1,19 @@
+// function reversedNum(num) {
+//   return parseFloat(num.toString().split("").reverse().join("")) * Math.sign(num);
+// }
+
+function reverseNum(x) {
+  var INT_MAX = 2147483647;
+  var INT_MIN = -INT_MAX - 1;
+  var res = 0;
+  var num = x;
+  while (num !== 0) {
+    res = res * 10 + (num % 10);
+    num = num > 0 ? Math.floor(num / 10) : Math.ceil(num / 10);
+    if (res > INT_MAX || res < INT_MIN) {
+      return 0;
+    }
+  }
+  return res;
+}
+console.log(reverseNum(123));
