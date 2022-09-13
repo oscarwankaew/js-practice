a4fabfb8a602382190343749e420fb444d2caf51
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-09-13 13:11:26 -0500

    solving longest prefix

---
+++ longest-prefix.js
@@ -0,0 +1,17 @@
+function longestCommonPrefix(strs) {
+  if (strs.length === 0) return "";
+  var len1 = strs.length;
+  var len2 = strs[0].length;
+  var tmp = "";
+  var res = "";
+  outer: for (var i = 0; i < len2; i++) {
+    tmp = strs[0][i];
+    inner: for (var j = 1; j < len1; j++) {
+      if (strs[j][i] !== tmp) break outer;
+    }
+    res += tmp;
+  }
+  return res;
+}
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
