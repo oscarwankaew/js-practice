70641328cbf0710d4d37dbcb615d92f787771f27
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-10-08 22:27:20 -0500

    solving remove element leetcode problem

---
+++ remove-element.js
@@ -0,0 +1,14 @@
+function removeElement(nums, val) {
+  if (!nums) {
+    return 0;
+  }
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums.length;
+}
+
+console.log(removeElement([3, 2, 2, 3], 3));
