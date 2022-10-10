df21cd2ab5f0e95d2ee6a3211e5f019cd809b0c3
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-10-09 21:42:26 -0500

    solving remove element problem using splice method

--- remove-element.js
+++ remove-element.js
@@ -2,13 +2,15 @@ function removeElement(nums, val) {
   if (!nums) {
     return 0;
   }
+  let pointer = 0;
+
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === val) {
-      nums.splice(i, 1);
-      i--;
+    if (nums[i] !== val) {
+      nums[pointer] = nums[i];
+      pointer++;
     }
   }
-  return nums.length;
+  return pointer;
 }
 
 console.log(removeElement([3, 2, 2, 3], 3));
