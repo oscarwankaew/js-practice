a4b99887c5e1ea5b5db7fe9d40de3e960881d6b2
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-10-11 15:13:10 -0500

    solving meeting rooms problem on leetcode

---
+++ meeting-rooms.js
@@ -0,0 +1,28 @@
+function meetingTimes(intervals) {
+  if (intervals.length < 2) {
+    return true;
+  }
+  intervals.sort((a, b) => {
+    return a[0] - b[0];
+  });
+
+  let end = intervals[0][1];
+
+  for (let i = 1; i < intervals.length; i++) {
+    if (end > intervals[i][0]) {
+      return false;
+    }
+    if (end < intervals[i][1]) {
+      end = intervals[i][1];
+    }
+  }
+  return true;
+}
+
+console.log(
+  meetingTimes([
+    [0, 30],
+    [5, 10],
+    [15, 20],
+  ])
+);
