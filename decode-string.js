03c16c11cf3e6697669ac1c04974f83827412596
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-10-13 15:02:54 -0500

    trying to solve decode string problem on leetcode

---
+++ decode-string.js
@@ -0,0 +1,25 @@
+function decode(string) {
+  let multiply = [];
+  let tempMulti = "";
+  let repeatStr = [];
+  let solution = "";
+  for (let char of string) {
+    if (!isNaN(char)) {
+      tempMulti = `${tempMulti}${char}`;
+    } else if (char === "[") {
+      multiply.push(tempMulti);
+      tempMulti = "";
+
+      repeatStr.push(solution);
+      solution = "";
+    } else if (char === "]") {
+      solution = repeatStr.pop() + solution.repeat(multiply.pop());
+    } else {
+      solution += char;
+    }
+  }
+
+  return solution;
+}
+
+console.log(decode(["3[a2[bc]]"]));
