616bc746980d290f9a14880b5da07dedbe0d6db5
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-12-19 13:22:02 -0600

    practicing arrays reduce using each loop

--- arrays_reduce_practice_each_solutions.js
+++ arrays_reduce_practice_each_solutions.js
@@ -63,3 +63,21 @@ items.forEach(function (item) {
   }
 });
 console.log(cheapestItem);
+
+//  7. Start with an array of numbers and compute product of all the numbers.
+//     For example, [5, 10, 8, 3] becomes 1200.
+var numbers = [5, 10, 8, 3];
+var product = 1;
+numbers.forEach(function (number) {
+  product = product * number;
+});
+console.log(product);
+
+//  8. Start with an array of strings and combine them all into a single string, separated by dashes.
+//     For example, ["volleyball", "basketball", "badminton"] becomes "-volleyball-basketball-badminton-".
+var strings = ["volleyball", "basketball", "badminton"];
+var singleString = "-";
+strings.forEach(function (string) {
+  singleString += string + "-";
+});
+console.log(singleString);
