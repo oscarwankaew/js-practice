f573619185deab470d9699a9a3f5fea9f7fd8e11
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-12-22 11:29:04 -0600

    practicing arrays reduce using while loop

--- array_reduce_practice_while_loop.js
+++ array_reduce_practice_while_loop.js
@@ -20,3 +20,32 @@ while (index < sports.length) {
   index = index + 1;
 }
 console.log(word);
+
+// 3. Start with an array of hashes and compute the sum of the prices (from the :price key).
+//    For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
+var items = [
+  { name: "chair", price: 100 },
+  { name: "pencil", price: 1 },
+  { name: "book", price: 4 },
+];
+var totalPrice = 0;
+var index = 0;
+while (index < items.length) {
+  totalPrice += items[index].price;
+  index += 1;
+}
+console.log(totalPrice);
+
+// 4. Start with an array of numbers and compute the the minumum number.
+//    For example, [5, 10, 8, 3] becomes 3.
+var numbers = [5, 10, 8, 3];
+var minimum = numbers[0];
+var index = 0;
+while (index < numbers.length) {
+  var number = numbers[index];
+  if (number < minimum) {
+    minimum = number;
+  }
+  index++;
+}
+console.log(minimum);
