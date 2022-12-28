cb6219ccf21bf43f21599a072ab3d005d1afb56e
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-12-27 22:10:24 -0600

    practicing arrays reduce using for each loop

--- array_reduce_practice_each.js
+++ array_reduce_practice_each.js
@@ -15,3 +15,27 @@ sports.forEach(function (sport) {
   word = word + sport;
 });
 console.log(word);
+
+//  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
+//     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
+var items = [
+  { name: "chair", price: 100 },
+  { name: "pencil", price: 1 },
+  { name: "book", price: 4 },
+];
+var totalPrice = 0;
+items.forEach(function (item) {
+  totalPrice += item.price;
+});
+console.log(totalPrice);
+
+//  4. Start with an array of numbers and compute the the minumum number.
+//     For example, [5, 10, 8, 3] becomes 3.
+var numbers = [5, 10, 8, 3];
+var minimum = numbers[0];
+numbers.forEach(function (number) {
+  if (number < minimum) {
+    minimum = number;
+  }
+});
+console.log(minimum);
