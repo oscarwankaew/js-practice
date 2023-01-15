230e10bd075fd741c613148b054986da9ffbfc66
Oscar Wankaew <oscar.wankaew@gmail.com>
2023-01-14 22:35:09 -0600

    added another way of solving convert to base problem on leetcode

--- convert-to-base.js
+++ convert-to-base.js
@@ -16,3 +16,13 @@ function negBase(val, base) {
   }
   return result;
 }
+
+// const baseNeg2 = function(N) {
+//   if (N === 0) return "0";
+//   let res = ''
+//   while(N !== 0) {
+//     res = (N & 1) + res
+//     N = -(N >> 1)
+//   }
+//   return res;
+// };
