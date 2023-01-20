996ea36ebe53548b7fd523de6060df4e31ef6d21
Oscar Wankaew <oscar.wankaew@gmail.com>
2023-01-19 21:44:23 -0600

    solving best siteseeing problem on leetcode

---
+++ best-siteseeing.js
@@ -0,0 +1,9 @@
+const maxScoreSightseeingPair = function (A) {
+  let res = 0,
+    cur = 0;
+  for (let a of A) {
+    res = Math.max(res, cur + a);
+    cur = Math.max(cur, a) - 1;
+  }
+  return res;
+};
