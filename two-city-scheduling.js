d46e462c202650d6e5542178d6be200239ae293b
Oscar Wankaew <oscar.wankaew@gmail.com>
2023-01-31 20:46:44 -0600

    another way solving two city scheduling problem on leetcode

--- two-city-scheduling.js
+++ two-city-scheduling.js
@@ -17,17 +17,35 @@
 
 // another
 
+// const twoCitySchedCost = function (costs) {
+//   const N = costs.length;
+//   let res = 0;
+//   const refund = [];
+//   for (let i = 0; i < N; i++) {
+//     refund[i] = costs[i][1] - costs[i][0];
+//     res += costs[i][0];
+//   }
+//   refund.sort((a, b) => a - b);
+//   for (let i = 0; i < N / 2; i++) {
+//     res += refund[i];
+//   }
+//   return res;
+// };
+
+// another
+
 const twoCitySchedCost = function (costs) {
-  const N = costs.length;
+  const len = costs.length;
+  if (len === 0) return 0;
+  const N = len / 2;
+  costs.sort((a, b) => a[0] - a[1] - (b[0] - b[1]));
   let res = 0;
-  const refund = [];
-  for (let i = 0; i < N; i++) {
-    refund[i] = costs[i][1] - costs[i][0];
-    res += costs[i][0];
-  }
-  refund.sort((a, b) => a - b);
-  for (let i = 0; i < N / 2; i++) {
-    res += refund[i];
+  for (let i = 0; i < costs.length; i++) {
+    if (i < N) {
+      res += costs[i][0];
+    } else {
+      res += costs[i][1];
+    }
   }
   return res;
 };
