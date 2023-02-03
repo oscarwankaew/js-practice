6f51d944afc4b4061f488cf1df67e8e03dc3fb52
Oscar Wankaew <oscar.wankaew@gmail.com>
2023-02-02 21:10:18 -0600

    Solving capacity to ship within d days 2

--- capacity-to-ship-packages-within-d-days.js
+++ capacity-to-ship-packages-within-d-days.js
@@ -1,25 +1,51 @@
-const shipWithinDays = function (weights, days) {
-  let l = Math.max(...weights);
-  let r = weights.reduce((ac, e) => ac + e, 0);
-  while (l < r) {
-    const mid = Math.floor((l + r) / 2);
-    if (valid(mid)) {
-      r = mid;
-    } else l = mid + 1;
+// const shipWithinDays = function (weights, days) {
+//   let l = Math.max(...weights);
+//   let r = weights.reduce((ac, e) => ac + e, 0);
+//   while (l < r) {
+//     const mid = Math.floor((l + r) / 2);
+//     if (valid(mid)) {
+//       r = mid;
+//     } else l = mid + 1;
+//   }
+
+//   return l;
+
+//   function valid(mid) {
+//     let res = 1,
+//       cur = 0;
+//     for (let w of weights) {
+//       if (cur + w > mid) {
+//         cur = 0;
+//         res++;
+//       }
+//       cur += w;
+//     }
+//     return res <= days;
+//   }
+// };
+
+// capacity to ship packages within d days 2
+
+const shipWithinDays = function (weights, D) {
+  let left = 0,
+    right = 0;
+  for (let w of weights) {
+    left = Math.max(left, w);
+    right += w;
   }
-
-  return l;
-
-  function valid(mid) {
-    let res = 1,
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2),
+      need = 1,
       cur = 0;
     for (let w of weights) {
       if (cur + w > mid) {
+        need += 1;
         cur = 0;
-        res++;
       }
       cur += w;
     }
-    return res <= days;
+    if (need > D) left = mid + 1;
+    else right = mid;
   }
+  return left;
 };
